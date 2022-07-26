8c459b86087d30c8b63be947d3ca3db2e98aa205
Sam <samjandris@gmail.com>
2022-07-25 20:15:01 -0400

    Locales, New Popup, File Restructure - changed to using locales for popup - new popup window with small styling added

--- js/popup.js
+++ js/popup.js
@@ -1,21 +1,37 @@
+document.querySelectorAll('[data-locale]').forEach((element) => {
+  element.innerText = chrome.i18n.getMessage(element.dataset.locale);
+});
+
 chrome.storage.sync.get((data) => {
   document.getElementById('miniPlayer').checked = data.miniPlayer;
   document.getElementById('returnDislikes').checked = data.returnDislikes;
+  document.getElementById('twitchTheater').checked = data.twitchTheater;
   document.getElementById('experimentalComments').checked =
     data.experimentalComments;
-  document.getElementById('twitchTheater').checked = data.twitchTheater;
 });
 
 document.getElementById('miniPlayer').addEventListener('change', () => {
   chrome.storage.sync.set({
     miniPlayer: document.getElementById('miniPlayer').checked,
   });
+
+  document.documentElement.setAttribute('refresh', '');
 });
 
 document.getElementById('returnDislikes').addEventListener('change', () => {
   chrome.storage.sync.set({
     returnDislikes: document.getElementById('returnDislikes').checked,
   });
+
+  document.documentElement.setAttribute('refresh', '');
+});
+
+document.getElementById('twitchTheater').addEventListener('change', () => {
+  chrome.storage.sync.set({
+    twitchTheater: document.getElementById('twitchTheater').checked,
+  });
+
+  document.documentElement.setAttribute('refresh', '');
 });
 
 document
@@ -25,10 +41,6 @@ document
       experimentalComments: document.getElementById('experimentalComments')
         .checked,
     });
-  });
 
-document.getElementById('twitchTheater').addEventListener('change', () => {
-  chrome.storage.sync.set({
-    twitchTheater: document.getElementById('twitchTheater').checked,
+    document.documentElement.setAttribute('refresh', '');
   });
-});
