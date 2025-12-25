357e98a7264d6bb55dfefa8d427654421307170f
Sam Jandris <samjandris@gmail.com>
2025-12-25 03:47:55 -0500

    dep bump, version bump

--- src/js/popup.ts
+++ src/js/popup.ts
@@ -1,80 +1,80 @@
-import '../css/popup.scss';
-import { STORAGE_DEFAULT } from './utils';
+import "../css/popup.scss";
+import { STORAGE_DEFAULT } from "./utils";
 
-document.querySelectorAll('[data-locale]').forEach((rawElem: Element) => {
+document.querySelectorAll("[data-locale]").forEach((rawElem: Element) => {
   const element = rawElem as HTMLElement;
 
   if (element.dataset.locale)
     element.innerText = chrome.i18n.getMessage(element.dataset.locale);
 });
 
-chrome.storage.sync.get(STORAGE_DEFAULT, (data) => {
-  (document.getElementById('miniPlayer') as HTMLInputElement).checked =
+chrome.storage.sync.get(STORAGE_DEFAULT, (data: typeof STORAGE_DEFAULT) => {
+  (document.getElementById("miniPlayer") as HTMLInputElement).checked =
     data.miniPlayer;
-  (document.getElementById('returnDislikes') as HTMLInputElement).checked =
+  (document.getElementById("returnDislikes") as HTMLInputElement).checked =
     data.returnDislikes;
-  (document.getElementById('twitchTheater') as HTMLInputElement).checked =
+  (document.getElementById("twitchTheater") as HTMLInputElement).checked =
     data.twitchTheater;
-  (document.getElementById('pipButton') as HTMLInputElement).checked =
+  (document.getElementById("pipButton") as HTMLInputElement).checked =
     data.pipButton;
   (
-    document.getElementById('experimentalComments') as HTMLInputElement
+    document.getElementById("experimentalComments") as HTMLInputElement
   ).checked = data.experimentalComments;
 });
 
-const miniPlayerElement = document.getElementById('miniPlayer');
+const miniPlayerElement = document.getElementById("miniPlayer");
 if (miniPlayerElement)
-  miniPlayerElement.addEventListener('change', () => {
+  miniPlayerElement.addEventListener("change", () => {
     chrome.storage.sync.set({
       miniPlayer: (miniPlayerElement as HTMLInputElement).checked,
     });
 
-    document.documentElement.setAttribute('refresh', '');
+    document.documentElement.setAttribute("refresh", "");
   });
 
-const returnDislikesElement = document.getElementById('returnDislikes');
+const returnDislikesElement = document.getElementById("returnDislikes");
 if (returnDislikesElement)
-  returnDislikesElement.addEventListener('change', () => {
+  returnDislikesElement.addEventListener("change", () => {
     chrome.storage.sync.set({
       returnDislikes: (returnDislikesElement as HTMLInputElement).checked,
     });
 
-    document.documentElement.setAttribute('refresh', '');
+    document.documentElement.setAttribute("refresh", "");
   });
 
-const twitchTheaterElement = document.getElementById('twitchTheater');
+const twitchTheaterElement = document.getElementById("twitchTheater");
 if (twitchTheaterElement)
-  twitchTheaterElement.addEventListener('change', () => {
+  twitchTheaterElement.addEventListener("change", () => {
     chrome.storage.sync.set({
       twitchTheater: (twitchTheaterElement as HTMLInputElement).checked,
     });
 
-    document.documentElement.setAttribute('refresh', '');
+    document.documentElement.setAttribute("refresh", "");
   });
 
-const testVideo = document.createElement('video');
+const testVideo = document.createElement("video");
 if (testVideo.requestPictureInPicture!)
-  document.documentElement.setAttribute('pip', '');
+  document.documentElement.setAttribute("pip", "");
 
-const pipButtonElement = document.getElementById('pipButton');
+const pipButtonElement = document.getElementById("pipButton");
 if (pipButtonElement)
-  pipButtonElement.addEventListener('change', () => {
+  pipButtonElement.addEventListener("change", () => {
     chrome.storage.sync.set({
       pipButton: (pipButtonElement as HTMLInputElement).checked,
     });
 
-    document.documentElement.setAttribute('refresh', '');
+    document.documentElement.setAttribute("refresh", "");
   });
 
 const experimentalCommentsElement = document.getElementById(
-  'experimentalComments'
+  "experimentalComments"
 );
 if (experimentalCommentsElement)
-  experimentalCommentsElement.addEventListener('change', () => {
+  experimentalCommentsElement.addEventListener("change", () => {
     chrome.storage.sync.set({
       experimentalComments: (experimentalCommentsElement as HTMLInputElement)
         .checked,
     });
 
-    document.documentElement.setAttribute('refresh', '');
+    document.documentElement.setAttribute("refresh", "");
   });
